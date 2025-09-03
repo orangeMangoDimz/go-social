e94ee44e7f6e19d92fca49cc5851383c59c6e1ad
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-09-03 16:25:54 +0700

    feat: add error utils handler

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Internal server error: %s path %s err %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, "Something went wrong")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request error: %s path %s err %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Not found error: %s path %s err %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
