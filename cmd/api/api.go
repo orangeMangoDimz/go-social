d45ec2b5b666b550cee2d74cf0163d8cb79582a3
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-09-09 14:27:48 +0700

    feat: add CORS config

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -12,10 +12,12 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"go.uber.org/zap"
 
 	"github.com/orangeMangoDimz/go-social/docs" // This is required to generate swagger docs
 	"github.com/orangeMangoDimz/go-social/internal/auth"
+	"github.com/orangeMangoDimz/go-social/internal/env"
 	"github.com/orangeMangoDimz/go-social/internal/mailer"
 	"github.com/orangeMangoDimz/go-social/internal/ratelimiter"
 	"github.com/orangeMangoDimz/go-social/store"
@@ -98,6 +100,14 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{env.GetString("CORS_ALLOWED_ORIGIN", "")},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	if app.config.rateLimiter.Enabled {
 		r.Use(app.RateLimiterMiddleware)
