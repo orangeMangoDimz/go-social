fad842e9db4f6a9cbe75093df19d82d380cb57f9
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-09-15 23:39:04 +0700

    chore: change comments folder on internal into service folder

--- internal/service/domain/service.go
+++ internal/service/domain/service.go
@@ -3,7 +3,7 @@ package domain
 import (
 	"github.com/orangeMangoDimz/go-social/internal/config"
 	"github.com/orangeMangoDimz/go-social/internal/service"
-	commentsService "github.com/orangeMangoDimz/go-social/internal/service/comments"
+	commentsService "github.com/orangeMangoDimz/go-social/internal/service/domain/comments"
 	followersService "github.com/orangeMangoDimz/go-social/internal/service/domain/followers"
 	postsService "github.com/orangeMangoDimz/go-social/internal/service/domain/posts"
 	rolesService "github.com/orangeMangoDimz/go-social/internal/service/domain/roles"
